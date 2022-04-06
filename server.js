ce4d943147b97c3410123323a7967819a72ac05f
Pyche Laveau <homedome@Seveonnos-MacBook-Air.local>
2022-04-06 10:11:16 -0700

    front-end i think..

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -50,9 +49,7 @@ function createNewAnimal(body, animalsArray) {
     path.join(__dirname, './data/animals.json'),
     JSON.stringify({ animals: animalsArray }, null, 2)
   );
-
-  // return finished code to post route for respose
-  return body;
+  return animal;
 }
 
 function validateAnimal(animal) {
@@ -88,18 +85,32 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
-app.post('/api/animals',(req, res) => {
+app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, sent 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
     const animal = createNewAnimal(req.body, animals);
-    res.json(req.body);
+    res.json(animal);
   }
-  
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
